ba15881d0d8b5325306d392ccbe4019130342543
Fan Yang <yangfcm16@gmail.com>
2020-06-29 23:23:27 +0930

    add button to header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -3,15 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   template: `
-    <div class="ui menu">
+    <div class="ui menu stackable">
       <a routerLink="/" class="header item ui">
         <i class="video icon red"></i>
         FanTube</a
       >
-      <div class="right item">
-        <div class="ui icon input">
-          <input type="text" placeholder="Search video..." />
-          <i class="search icon link" (click)="handleSearch()"></i>
+      <div class="right menu">
+        <div class="item">
+          <div class="ui icon input" style="min-width: 10px;">
+            <input type="text" placeholder="Search video..." />
+            <i class="search icon link" (click)="handleSearch()"></i>
+          </div>
+          <div>
+            <button
+              class="ui button primary"
+              style="margin-left: 1rem; width: 110px;"
+            >
+              <i class="sign in alternate icon"></i>
+              Sign in
+            </button>
+          </div>
         </div>
       </div>
     </div>
