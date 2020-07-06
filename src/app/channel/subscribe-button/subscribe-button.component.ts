ae617b18bb6fc59a348f82cca5a26e88a17cf682
Fan Yang <yangfcm16@gmail.com>
2020-07-06 21:13:02 +0930

    subscribe/unsubscribe channel

--- src/app/channel/subscribe-button/subscribe-button.component.ts
+++ src/app/channel/subscribe-button/subscribe-button.component.ts
@@ -2,11 +2,28 @@ import { Component, OnInit, Input, NgZone } from '@angular/core';
 import { ChannelService } from '../channel.service';
 import { GoogleAuthService, IAuth } from '../../auth/google-auth.service';
 
+enum ButtonText {
+  Unsubscribe = 'Unsubscribe',
+  Subscribe = 'Subscribe',
+  Subscribing = 'Subscribing',
+  Unsubscribing = 'Unsubscribing',
+  Subscribed = 'Subscribed',
+  Unsubscribed = 'Unsubscribed',
+}
+
 @Component({
   selector: 'app-subscribe-button',
   template: `
-    <button class="ui inverted red button">
-      {{ subscriptionId ? 'Subscribed' : 'Unsubscribed' }}
+    <button
+      class="ui inverted button"
+      [class]="subscriptionId ? 'red' : 'orange'"
+      *ngIf="auth && auth.signedIn && subscriptionId !== undefined"
+      (mouseover)="handleMouseOver()"
+      (mouseleave)="handleMouseLeave()"
+      (click)="subscriptionId ? handleUnsubscribe() : handleSubscribe()"
+      style="width: 120px;"
+    >
+      {{ buttonText }}
     </button>
   `,
   styles: [],
@@ -16,6 +33,7 @@ export class SubscribeButtonComponent implements OnInit {
   subscriptionId: string;
   auth: IAuth;
   isProcessing = false;
+  buttonText: ButtonText;
 
   constructor(
     private channelService: ChannelService,
@@ -49,6 +67,47 @@ export class SubscribeButtonComponent implements OnInit {
       .subscribe((data: string) => {
         this.subscriptionId = data;
         console.log(this.subscriptionId);
+        if (this.subscriptionId) {
+          this.buttonText = ButtonText.Subscribed;
+        } else {
+          this.buttonText = ButtonText.Unsubscribed;
+        }
       });
+  }
+
+  handleMouseOver() {
+    if (this.buttonText === 'Subscribed') {
+      this.buttonText = ButtonText.Unsubscribe;
+    }
+    if (this.buttonText === 'Unsubscribed') {
+      this.buttonText = ButtonText.Subscribe;
+    }
+  }
+
+  handleMouseLeave() {
+    if (this.buttonText === 'Subscribe') {
+      this.buttonText = ButtonText.Unsubscribed;
+    }
+    if (this.buttonText === 'Unsubscribe') {
+      this.buttonText = ButtonText.Subscribed;
+    }
+  }
+
+  handleUnsubscribe() {
+    this.buttonText = ButtonText.Unsubscribing;
+    this.channelService
+      .unsubscribeChannel(this.subscriptionId)
+      .subscribe(() => {
+        this.subscriptionId = '';
+        this.buttonText = ButtonText.Unsubscribed;
+      });
+  }
+
+  handleSubscribe() {
+    this.buttonText = ButtonText.Subscribing;
+    this.channelService.subscribeChannel(this.channelId).subscribe((data) => {
+      this.fetchSubscriptionId();
+      this.buttonText = ButtonText.Subscribed;
+    });
   }
 }
